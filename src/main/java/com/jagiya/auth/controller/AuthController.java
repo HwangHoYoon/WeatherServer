f48b06befd4250b9d435f06e2e23013f52c013f3
황호윤 <hoyoon9227@gmail.com>
2023-09-02 21:27:25 +0900

    카카오 로그인 1차 테스트용

--- src/main/java/com/jagiya/auth/controller/AuthController.java
+++ src/main/java/com/jagiya/auth/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.jagiya.auth.controller;
 
 import com.jagiya.auth.dto.UsersRes;
 import com.jagiya.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
+@Tag(name = "auth", description = "소셜 로그인")
 public class AuthController {
 
     private final AuthService signUpService;

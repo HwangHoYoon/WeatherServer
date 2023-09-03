f03afeeacb693ea1ca8ea077e96333f32f63ffd6
sun035160 <sun035160@gmail.com>
2023-09-04 01:55:10 +0900

    Apple Controller Update

--- src/main/java/com/jagiya/main/controller/AppleController.java
+++ src/main/java/com/jagiya/main/controller/AppleController.java
@@ -17,7 +17,7 @@ public class AppleController {
 
     private final AppleService appleService;
 
-    @PostMapping("/callback")
+    @PostMapping("/apple/callback")
     public ResponseEntity<MsgEntity> callback(HttpServletRequest request) throws Exception {
         AppleDTO appleInfo = appleService.getAppleInfo(request.getParameter("code"));
 

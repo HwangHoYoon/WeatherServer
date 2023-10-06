1a87c63da291be2552c474536ce6774e3b842f1e
황호윤 <hoyoon9227@gmail.com>
2023-10-06 21:15:40 +0900

    주소목록 response 속성 이름 컬럼명으로 나오게 변경

--- src/main/java/com/jagiya/location/response/LocationResponse.java
+++ src/main/java/com/jagiya/location/response/LocationResponse.java
@@ -1,22 +1,23 @@
 package com.jagiya.location.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import lombok.*;
 
 @Data
 public class LocationResponse {
 
-    @JsonProperty("시도명")
+    @Schema(description = "시도명)", example = "경기도", name = "cityDo")
     private String cityDo;
 
-    @JsonProperty("시군구명")
+    @Schema(description = "시군구명", example = "부천시", name = "guGun")
     private String guGun;
 
-    @JsonProperty("읍면동명")
+    @Schema(description = "읍면동명", example = "상동", name = "eupMyun")
     private String eupMyun;
 
-    @JsonProperty("행정구역코드")
+    @Schema(description = "행정구역코드", example = "4119010900", name = "regionCd")
     private String regionCd;
 
 }
